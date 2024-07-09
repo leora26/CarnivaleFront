ea0d2f95c185dd1995d3d1d3036bda84d0ac9685
Leonid  Romaniukha <leonid.romaniukha@student.kdg.be>
2024-07-09 14:22:53 +0200

    changed folder for build:v2

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.tsx',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'build'),
         publicPath: '/CarnivaleFront/'
     },
     resolve: {
