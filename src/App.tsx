155289f6049138bf179dc0d784aa56ecd01cb172
Leonid  Romaniukha <leonid.romaniukha@student.kdg.be>
2024-07-15 15:40:56 +0200

    added tests for ProductCard and StoraCard components

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import DefaultLayout from "./layout/DefaultLayout";
 import HomePage from "./pages/static/HomePage";
 import { ThemeProvider } from "styled-components";
 import theme from "./components/styled/theme";
+import GlobalStyles from "./components/styled/Global";
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
+            <GlobalStyles />
                 <Routes>
                     <Route path="" element={<DefaultLayout />}>
                         <Route index element={<HomePage />} />
---
+++ src/tests/StoryCard.test.tsx
@@ -0,0 +1,37 @@
+import {render, screen} from "@testing-library/react";
+import {ThemeProvider} from "styled-components";
+import theme from "../components/styled/theme";
+import AllStories from "../models/AllStories";
+import MediaContent from "../models/MediaContent";
+import StoryCard from "../components/StoryCard";
+
+describe("ProductCard component", () => {
+    test("should render product", () => {
+        const story = new AllStories(
+            1,
+            "Test story",
+            "this is a test story",
+            "10/12/2015",
+            [new MediaContent(
+                1,
+                "https://via.placeholder.com/150x150",
+                "Image",
+            )],
+        );
+        render(
+            <ThemeProvider theme={theme}>
+                <StoryCard story={story} />
+            </ThemeProvider>
+        );
+
+        const storyImage = screen.getByRole("img") as HTMLImageElement;
+        expect(storyImage.src).toBe(story.mediaContents[0].url);
+
+        const storyTitle = screen.getByText(story.title);
+        expect(storyTitle).toBeInTheDocument();
+
+        const storySubTitle = screen.getByText(story.subTitle);
+        expect(storySubTitle).toBeInTheDocument();
+
+    })
+})
