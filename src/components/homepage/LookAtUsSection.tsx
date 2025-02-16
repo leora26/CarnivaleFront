c433e88fef3461f081c57f89b1da37dbedd63e1b
Leonid  Romaniukha <leonid.romaniukha@student.kdg.be>
2025-02-16 13:45:08 +0100

    look at us section

--- src/components/homepage/LookAtUsSection.tsx
+++ src/components/homepage/LookAtUsSection.tsx
@@ -1,7 +1,11 @@
-import {ImagesContainer, LookAtUsContainer, LookAtUsSectionStyled} from "../styled/homepage/LookAtUsSection.styled";
+import {
+    BigImageContainer,
+    ImagesContainer,
+    LookAtUsContainer,
+    LookAtUsSectionStyled
+} from "../styled/homepage/LookAtUsSection.styled";
 import React, {useContext} from "react";
 import {HomepageContext} from "../../context/HomepageContext";
-import {AiOutlinePlus} from "react-icons/ai";
 import {IoArrowForward} from "react-icons/io5";
 
 const LookAtUsSection = () => {
@@ -16,31 +20,36 @@ const LookAtUsSection = () => {
 
             <LookAtUsContainer>
 
-                <ImagesContainer direction="column">
+                <BigImageContainer direction="column" id="firstImages" className="bigSection">
 
-                    <img src={homepageInfo?.homepageInfo?.gif1} alt="First gif"/>
+                    <img src={homepageInfo?.homepageInfo?.img1} id="firstImage" alt="First image"/>
 
-                    <ImagesContainer direction='row'>
-                        <img src={homepageInfo?.homepageInfo?.img1} alt="First image"/>
-                        <button>See more <IoArrowForward id="seeMoreIcon"/></button>
+                    <ImagesContainer direction='row' id="imageAndButton">
+                        <img src={homepageInfo?.homepageInfo?.img2} id="secondImage" alt="Second image"/>
+                        <button id="seeMoreButton">
+                            <div id="buttonContainer">
+                                <span>See more </span>
+                                <IoArrowForward id="seeMoreIcon"/>
+                            </div>
+                        </button>
                     </ImagesContainer>
 
-                </ImagesContainer>
+                </BigImageContainer>
 
-                <ImagesContainer direction="column">
-                    <img src={homepageInfo?.homepageInfo?.gif2} alt="Second gif"/>
-                </ImagesContainer>
+                <BigImageContainer direction="column" id="bigGif">
+                    <img src={homepageInfo?.homepageInfo?.gif1} id="firstGif" alt="First gif"/>
+                </BigImageContainer>
 
-                <ImagesContainer direction="column">
+                <BigImageContainer direction="column" id="secondImages">
 
-                    <ImagesContainer direction="row">
-                        <img src={homepageInfo?.homepageInfo?.img2} alt="Second gif"/>
-                        <img src={homepageInfo?.homepageInfo?.img3} alt="Second gif"/>
+                    <ImagesContainer direction="row" id="smallestImages">
+                        <img src={homepageInfo?.homepageInfo?.gif2} id="secondGif" alt="Second gif"/>
+                        <img src={homepageInfo?.homepageInfo?.img3} id="thirdImage" alt="Third image"/>
                     </ImagesContainer>
 
-                    <img src={homepageInfo?.homepageInfo?.img4} alt="Second gif"/>
+                    <img src={homepageInfo?.homepageInfo?.img4} id="fourthImage" alt="Fourth image"/>
 
-                </ImagesContainer>
+                </BigImageContainer>
 
             </LookAtUsContainer>
 
--- src/components/styled/homepage/LookAtUsSection.styled.ts
+++ src/components/styled/homepage/LookAtUsSection.styled.ts
@@ -4,8 +4,8 @@ export const LookAtUsSectionStyled = styled.section`
     display: flex;
     flex-direction: column;
     width: 100%;
-    height: 80vh;
-    align-items: start;
+    min-height: 80vh;
+    align-items: center;
     margin-bottom: 5%;
     
     h3 {
@@ -13,12 +13,60 @@ export const LookAtUsSectionStyled = styled.section`
         text-transform: uppercase;
         font-family: ${({ theme }) => theme.fonts.primaryBold};
         font-weight: bold;
+        align-self: start;
+        margin-bottom: 2rem;
+        margin-left: 5%;
     }
 `
 
 export const LookAtUsContainer = styled.div`
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 90%;
+    
+    img {
+        border-radius: 1rem;
+    }
+    
+    #firstImages {
+        width: 38rem;
+        height: 100%;
+        
+        #firstImage {
+            margin-bottom: 1rem;
+        }
+        
+        #imageAndButton{
+            justify-content: space-between;
+            
+            img {
+                width: 80%;
+            }
+        }
+    }
+    
+    #bigGif {
+        width: 24rem;
+    }
+    
+    #secondImages{
+        width: 30rem;
+        height: 100%;
+
+        
+        #smallestImages{
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 1rem;
+            
+            img {
+                width: 48%;
+            }
+        }
+    }
+    
 `
 
 
@@ -30,5 +78,44 @@ export const ImagesContainer = styled.div<ImagesContainerProps>`
     display: flex;
     flex-direction: ${({direction}) => direction};
     
+    
+    #seeMoreButton {
+        margin-left: 1rem;
+        font-weight: bold;
+        cursor: pointer;
+        width: 10%;
+        background-color: ${({theme}) => theme.colors.primary};
+        border: none;
+        border-radius: 5rem;
+        
+        #buttonContainer {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-evenly;
+            align-items: center;
+            transform: translate(-50%, -50%) rotate(-90deg);
+            width: 20rem;
+            margin-left: 1.5rem;
+            margin-top: 2rem;
+        }
+        
+        span {
+            display: inline-block;
+            white-space: nowrap; 
+            font-size: 1.8rem;
+        }
+
+        svg {
+            width: 50px;
+            height: 50px;
+            transform:rotate(45deg);
+        }
+    }
+    
+`
+
+export const BigImageContainer = styled.div<ImagesContainerProps>`
+    display: flex;
+    flex-direction: ${({direction}) => direction};
     
 `
